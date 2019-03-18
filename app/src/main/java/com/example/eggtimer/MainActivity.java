a44dd51d57299ae40934d1355d8e351e7e8886b8
zxcv1337x <michaelhaggard94@gmail.com>
2019-03-18 16:04:37 -0500

    swear i pushed this earlier

---
+++ app/src/main/java/com/example/eggtimer/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.eggtimer;
+
+import android.media.MediaPlayer;
+import android.os.CountDownTimer;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView timerTextView;
+    SeekBar timerSeekBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        timerSeekBar = findViewById(R.id.timerSeekBar);
+        timerTextView = findViewById(R.id.timerTextView);
+
+        timerSeekBar.setMax(600);
+        timerSeekBar.setProgress(30);
+
+
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                updateTimer(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public void buttonClicked(View view) {
+        CountDownTimer countDownTimer = new CountDownTimer(timerSeekBar.getProgress() * 1000, 1000) {
+            @Override
+            public void onTick(long l) {
+                updateTimer((int) l / 1000);
+            }
+
+            @Override
+            public void onFinish() {
+                MediaPlayer mplayer = MediaPlayer.create(getApplicationContext(),R.raw.airhorn);
+                mplayer.start();
+            }
+        }.start();
+    }
+
+    public void updateTimer(int secondsLeft) {
+        int minutes = secondsLeft / 60;
+        int seconds = secondsLeft - (minutes * 60);
+
+        String secondString = Integer.toString(seconds);
+
+        if(seconds <= 9) {
+            secondString = "0" + secondString;
+        }
+
+        timerTextView.setText(Integer.toString(minutes) + ":" + secondString);
+    }
+}
